f5c0a53034720d7ab1a61fe1febbc5f1d96040ce
omidbaharifar <weblax.ir@gmail.com>
2023-12-22 22:16:10 +0330

    feat: new eps added

---
+++ e13/src/routes/index.tsx
@@ -0,0 +1,25 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+
+import Counter from "~/components/starter/counter/counter";
+import Hero from "~/components/starter/hero/hero";
+import Infobox from "~/components/starter/infobox/infobox";
+import Starter from "~/components/starter/next-steps/next-steps";
+
+export default component$(() => {
+  return (
+    <>
+      <h1 class="bg-red-500">اینجا کدنایته!</h1>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Welcome to Qwik",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik site description",
+    },
+  ],
+};
